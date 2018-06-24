df85e3f29e54bf48fe0ce7b6d8b33b92adacd5c5
javaterminology <vrajasekharreddy@gmail.com>
2018-06-24 17:09:24 +0800

    apache-kafka-queue

---
+++ kafka-queue/src/main/java/com/terminology/kafka/App.java
@@ -0,0 +1,13 @@
+package com.terminology.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
